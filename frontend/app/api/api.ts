5ac7fe4b757315b02cf0123433b784ace2f2ffab
rakib151p <rakib151p@gmail.com>
2025-07-16 14:12:06 +0600

    fix: Base Url

--- frontend/app/api/api.ts
+++ frontend/app/api/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 // const BASE_URL = 'http://127.0.0.1:8000';
-const BASE_URL = 'http://40.81.16.202:8000/';
+// const BASE_URL = 'http://40.81.16.202:8000/';
+const BASE_URL = 'https://django-react-todo-application.onrender.com';
 
 function setCookie(name: string, value: string, days = 7) {
   const expires = new Date(Date.now() + days * 864e5).toUTCString();
